972b8df7723e4890994b0e10ac34cd6b903702ed
Kline <527811545@qq.com>
2023-10-17 12:29:20 +0800

    完善readme文件，增添多个后续开发的接口

--- models/product.js
+++ models/product.js
@@ -9,8 +9,29 @@ class Product extends Base {
   
   async createProduct (nft_identifier, product_name, product_description, url, owner_id) {
     const product_id = uuid.v4(); // 生成唯一的 product_id
-    return this.insert({ product_id, nft_identifier, product_name, product_description, url, owner_id });
+    return this.insert({ product_id, nft_identifier, product_name, product_description, url, owner_id }); // 此处没有插入buyer_id，应该会默认设为NULL，但如果插入出错可以检查这里
   }
+
+  // Get details of a single NFT
+  async getNFTDetails(nftId) {
+    // Implement DB query to fetch details of a specific NFT using nftId
+  }
+
+  // List NFTs owned by a user
+  async listUserNFTs(userId) {
+    // Implement DB query to list all NFTs owned by a specific user
+  }
+
+  // List NFTs for sale by a user
+  async listUserSaleNFTs(userId) {
+    // Implement DB query to list all NFTs for sale by a specific user
+  }
+
+  // Search for NFTs
+  async searchNFTs(query) {
+    // Implement DB query to search NFTs based on the given query
+  }
+
 }
 
 module.exports = new Product();
