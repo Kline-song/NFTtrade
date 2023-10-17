3ed20b50f1a621932a907cee20461783752f1451
zqh0703 <2269063498@qq.com>
2023-10-17 14:58:28 +0800

    1.0

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -9,9 +9,10 @@ const orderController = {
     // 以下是数据库插入的可能示例，但我不确定它的正确性
     // const userData = await createorder (product_id, seller_id, order_amount);
   },
-  
+
   // 展示一个订单
   // ### 请你补充或完善 ###
+
 
   // 有关订单的其他方法
   // ### 请你补充或完善 ###
