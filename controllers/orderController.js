5b354ed21ce7a497bcd67aec9be4d6ca301cf844
zqh0703 <2269063498@qq.com>
2023-10-18 17:20:11 +0800

    1.0-接口

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,19 +1,190 @@
 // orderController
 
 const Order = require('../models/order.js');
+const user = require('../models/user.js');
+
+// 以下为未实现的函数伪代码
 
 const orderController = {
+
   // 创建一个订单
   createOrder: async function (req, res, next) {
-    // ### 请你补充或完善 ###
-    // 以下是数据库插入的可能示例，但我不确定它的正确性
-    // const userData = await createorder (product_id, seller_id, order_amount);
+    try {
+      const { seller_id, product_id, order_amount } = req.body;
+
+
+      // 创建订单
+      const order = await Order.createOrder(product_id, seller_id, order_amount);
+
+      await order.save();
+
+      res.json({ code: 200, message: '订单创建成功', data: order });
+    } catch (error) {
+      res.status(500).json({ code: 500, message: '创建订单时发生错误', error: error.message });
+    }
   },
 
   // 展示一个订单
-  // ### 请你补充或完善 ###
+  showOrder: async function (req, res, next) {
+    try {
+      // 获取订单ID
+      const order_id = req.params.order_id;
 
+      // 从数据库中获取订单数据
+      const order = await Order.findById(order_id);
 
+      if (!order) {
+        // 如果订单不存在，则返回相应的错误响应
+        return res.status(404).json({
+          code: 404,
+          message: '订单未找到'
+        });
+      }
+      // 返回订单数据给用户
+      res.status(200).json({
+        code: 200,
+        message: '订单获取成功',
+        data: order
+      });
+    } catch (error) {
+      res.status(500).json({
+        code: 500,
+        message: '获取订单时发生错误',
+        error: error.message
+      });
+    }
+  },
+
+  //取消一个订单
+  cancelOrder: async function (req, res, next) {
+    try {
+      // 获取订单ID
+      const order_id = req.params.order_id;
+
+      // 从数据库中获取订单数据
+      const order = await Order.findById(order_id);
+
+      if (!order) {
+        return res.status(404).json({
+          code: 404,
+          message: '订单未找到'
+        });
+      }
+
+      if (order.order_status === 0) {
+        return res.status(400).json({
+          code: 400,
+          message: '订单已取消'
+        });
+      }
+
+      order.order_status = 0;
+      await order.save();
+
+      res.status(200).json({
+        code: 200,
+        message: '订单已成功取消',
+        data: order
+      });
+    } catch (error) {
+      res.status(500).json({
+        code: 500,
+        message: '取消订单时发生错误',
+        error: error.message
+      });
+    }
+  },
+
+  //交易一个订单
+  getTransactionOrder: async function (req, res, next) {
+    try {
+      const { order_id } = req.params;
+
+      // 从数据库中获取订单数据
+      const order = await Order.findById(order_id);
+
+      if (!order) {
+        return res.status(404).json({
+          code: 404,
+          message: '订单未找到'
+        });
+      }
+
+      // 处理交易过程
+      // 减少买家资产（假设有相关的买家资产字段）
+      const buyer = await user.findById(order.buyer_id);
+      buyer.currancy -= order.order_amount;
+      await buyer.save();
+
+      // 增加卖家资产（假设有相关的卖家资产字段）
+      const seller = await user.findById(order.seller_id);
+      seller.currancy += order.order_amount;
+      await seller.save();
+
+      // 修改商品归属权（假设有相关的商品归属权字段）
+      const product = await product.findById(order.product_id);
+      product.owner_id = order.buyer_id;
+      await product.save();
+
+      // 更新订单状态和买家ID
+      order.order_status = 2;  // 假设状态2表示交易完成
+      order.buyer_id = req.body.buyer_id;
+      await order.save();
+
+      // 返回订单信息和处理结果
+      res.status(200).json({
+        code: 200,
+        message: '交易处理成功',
+        data: order
+      });
+    } catch (error) {
+      res.status(500).json({
+        code: 500,
+        message: '处理交易时发生错误',
+        error: error.message
+      });
+    }
+  },
+
+  //将订单加入购物车
+  pending_paymentOrder: async function (req, res, next) {
+    try {
+      // 获取订单ID
+      const order_id = req.params.order_id;
+
+      // 从数据库中获取订单数据
+      const order = await Order.findById(order_id);
+
+      if (!order) {
+        return res.status(404).json({
+          code: 404,
+          message: '订单未找到'
+        });
+      }
+
+      if (order.order_status === 3) {
+        return res.status(400).json({
+          code: 400,
+          message: '订单已加入购物车'
+        });
+      }
+
+      order.order_status = 3;
+      await order.save();
+
+      res.status(200).json({
+        code: 200,
+        message: '订单已成功加入购物车',
+        data: order
+      });
+    } catch (error) {
+      res.status(500).json({
+        code: 500,
+        message: '订单加入购物车时发生错误',
+        error: error.message
+      });
+    }
+  },
   // 有关订单的其他方法
   // ### 请你补充或完善 ###
 }
--- models/order.js
+++ models/order.js
@@ -1,15 +1,15 @@
 const Base = require('./base');
 const uuid = require('uuid');
 
-class Order extends Base { 
+class Order extends Base {
   // 定义参数默认值为 order 表
-  constructor(props = 'order'){
+  constructor(props = 'order') {
     super(props);
   }
-  
-  async createorder (product_id, seller_id, order_amount) {
-    const order_id = uuid.v4(); // 生成唯一的 product_id
-    const order_status = 1; // order_status只有3个值，0 已取消，1 待交易，2 已交易
+
+  async createorder(product_id, seller_id, order_amount) {
+    const order_id = uuid.v4(); // 生成唯一的 order_id
+    const order_status = 1; // order_status只有4个值，0 已取消，1 待交易，2 已交易，3 待付款
     const create_time = new Date().toISOString().slice(0, 19).replace('T', ' ');
     return this.insert({ order_id, order_status, create_time, product_id, seller_id, order_amount });
   }
@@ -20,7 +20,9 @@ class Order extends Base {
   async getNFTTransactionHistory(nftId) {
     // Implement DB query to fetch transaction history of a specific NFT using nftId
   }
-
+  async findById(order_id) {
+    return this.where({ order_id }).find();
+  }
 }
 
 module.exports = new Order();
