d268a5283f424c02f937b2dc292ea4faf1f046e1
Kline-song <527811545@qq.com>
2023-11-17 16:23:37 +0800

    product修订

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,6 +6,8 @@ const pinFileToIPFS = require('../IPFS.js'); // 将文件上传至IPFS
 const configs = require('../config'); // 引用配置文件
 const path = require('path'); // 用于处理文件路径
 const fs = require('fs'); // 
+const uuid = require('uuid');
+const nftService = require('../services/nftService.js');
 
 const productController = {
   // 上传产品 syh
@@ -21,7 +23,6 @@ const productController = {
 
       // 处理NFT元数据文件
       const metaDataFile = req.files.metadata[0];
-
 
       // // 测试代码
       // console.log('req.files:', req.files);
@@ -50,44 +51,45 @@ const productController = {
       const metaDataIpfsGateway = configs.ipfs.gateway;
       const metaData_url = metaDataIpfsGateway + metaDataIpfsHash;
 
-      // 获取当前用户的 user_id
-      const owner_id = req.session.user_id;
-
       // 获取上传的表单数据
-      const { product_name, product_description } = req.body;
+      const { product_name, product_description, privateKey } = req.body;
 
-    //   const nftData = {
-    //     name: product_name,
-    //     description: product_description,
-    //     uri: metaData_url,
-    //     owner: owner_id
-    //   };
+      // 生成唯一的 nft_id
+      const nft_id = uuid.v4();
+
+      //   const nftData = {
+      //     name: product_name,
+      //     description: product_description,
+      //     uri: metaData_url,
+      //     owner: owner_id
+      //   };
 
       // 发送请求到rchain，创建NFT
       // var nft_id = await nftService.mint(product_name, product_description, metaData_url, owner_id);
-      const rchainResponse = {
-        status: 200,
-        data: {
-          transaction_status: 'success',
-          nft_identifier: "test"
-        }
-      };
+      // const rchainResponse = {
+      //   status: 200,
+      //   data: {
+      //     transaction_status: 'success',
+      //     nft_identifier: "test"
+      //   }
+      // };
 
-      // 检查HTTP响应状态
-      if (rchainResponse.status !== 200) {
-        const errorMsg = rchainResponse.data && rchainResponse.data.error ? rchainResponse.data.error : '未知错误';
-        return res.status(400).json({ code: 400, message: '与rchain交互时出错', detail: errorMsg });
-      }
+      // // 检查HTTP响应状态
+      // if (rchainResponse.status !== 200) {
+      //   const errorMsg = rchainResponse.data && rchainResponse.data.error ? rchainResponse.data.error : '未知错误';
+      //   return res.status(400).json({ code: 400, message: '与rchain交互时出错', detail: errorMsg });
+      // }
 
-      const { transaction_status, nft_identifier } = rchainResponse.data;
+      // const { transaction_status, nft_identifier } = rchainResponse.data;
 
-      // 检查返回的数据状态
-      if (transaction_status !== 'success' || !nft_identifier) {
-        const errorMsg = rchainResponse.data && rchainResponse.data.error ? rchainResponse.data.error : '未知错误';
-        return res.status(400).json({ code: 400, message: '创建nft_identifier失败', detail: errorMsg });
-      }
+      // // 检查返回的数据状态
+      // if (transaction_status !== 'success' || !nft_identifier) {
+      //   const errorMsg = rchainResponse.data && rchainResponse.data.error ? rchainResponse.data.error : '未知错误';
+      //   return res.status(400).json({ code: 400, message: '创建nft_identifier失败', detail: errorMsg });
+      // }
 
-      const productData = await Product.createProduct(nft_identifier, product_name, product_description, metaData_url, coverImage_url, owner_id);
+      const productData = await nftService.mint(privateKey, nft_id, product_name, product_description, metaData_url, coverImage_url);
+
       res.status(200).json({ code: 200, message: '产品上传成功', data: productData });
 
     } catch (error) {
@@ -105,102 +107,119 @@ const productController = {
   //     res.status(500).json({ code: 500, message: '获取产品时发生错误', error: error.message });
   //   }
   // },
+    //展示待出售的全部商品 syh
+    listProductsForSale: async function (req, res, next) {
+      try {
+      //   const orders = await Order.findOrdersByStatus(1);
+      // // console.log(orders);
+      // const productIds = orders.map(order => order.product_id);
+      // // console.log(productIds);
+      // let products = await Product.findProductsByIds(productIds);
+      // products = products.map(product => {
+      //   const correspondingOrder = orders.find(order => order.product_id === product.product_id);
+      //   if (correspondingOrder) {
+      //     product.order_amount = correspondingOrder.order_amount;
+      //   }
+      //   return product;
+        const nftsForSale = await nftService.listNftsForSale();
 
-  //展示待出售的全部商品 syh
-  listProductsForSale: async function (req, res, next) {
-    // 改为查询NFT的价格
-    try {
-      const orders = await Order.findOrdersByStatus(1);
-      // console.log(orders);
-      const productIds = orders.map(order => order.product_id);
-      // console.log(productIds);
-      let products = await Product.findProductsByIds(productIds);
-      products = products.map(product => {
-        const correspondingOrder = orders.find(order => order.product_id === product.product_id);
-        if (correspondingOrder) {
-          product.order_amount = correspondingOrder.order_amount;
-        }
-        return product;
-      });
-      // console.log(products);
-      res.json(products);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  },
-  //查看单个商品详情 (Get details of a single product) cxy
-  getProductDetails: async function (req, res, next) {
-    const productId = req.params.id;
-    try {
-      const productDetails = await Product.findById(productId);
-      // console.log(productDetails);
-      if (!productDetails) {
-        // 如果没有找到产品，返回 404 状态码
-        res.status(404).send({ message: 'Product not found' });
-      } else {
-        // 如果找到了产品，返回产品数据
-        res.send(productDetails);
+        // 处理返回的NFT数据以符合现有产品数据结构
+        let products = nftsForSale.map(nft => {
+          return {
+            product_id: nft.id, // 假设NFT的id对应于product_id
+            name: nft.name,
+            description: nft.description,
+            metadataUrl: nft.metadataUrl,
+            coverImgUrl: nft.coverImgUrl,
+            creator: nft.creator,
+            price: nft.price,
+            // 可能需要添加更多字段以符合Product模型
+          };
+        });
+
+        // 返回处理过的产品列表
+        res.json(products);
+
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
       }
-    } catch (error) {
-      // 如果有错误，返回 500 状态码和错误信息
-      console.error(error);
-      res.status(500).send({ message: 'Server error' });
-    }
-  },
+    },
 
-  // 列出用户的products(List products owned by a user) cxy
-  listUserProducts: async function (req, res, next) {
-    const userId = req.session.user_id;
-    try {
-      const products = await Product.listBy({ owner_id: userId });
-      res.status(200).json({ code: 200, data: products });
-    } catch (error) {
-      res.status(500).json({ code: 500, message: '获取用户产品时发生错误', error: error.message });
-    }
-  },
 
-  // // 列出用户发布的待售products (List products for sale by a user)
-  // listUserSaleNFTs: async function (req, res, next) {
-  //   // 涉及到用户id的使用，路由中应该加入登录检查中间件
-  //   const userId = req.session.user_id;
-  //   try {
-  //     // 获取当前用户作为卖家的全部订单
-  //     const orders = await Order.listBy({ seller_id: userId });
+    //查看单个商品详情 (Get details of a single product) cxy
+    getProductDetails: async function (req, res, next) {
+      const productId = req.params.id;
+      try {
+        const productDetails = await Product.findById(productId);
+        // console.log(productDetails);
+        if (!productDetails) {
+          // 如果没有找到产品，返回 404 状态码
+          res.status(404).send({ message: 'Product not found' });
+        } else {
+          // 如果找到了产品，返回产品数据
+          res.send(productDetails);
+        }
+      } catch (error) {
+        // 如果有错误，返回 500 状态码和错误信息
+        console.error(error);
+        res.status(500).send({ message: 'Server error' });
+      }
+    },
 
-  //     // 将待售物品id提取出来放入一个数组中
-  //     const productIdsForSale = orders.map(o => o.product_id);
+    // 列出用户的products(List products owned by a user) cxy
+    listUserProducts: async function (req, res, next) {
+      const userId = req.session.user_id;
+      try {
+        const products = await Product.listBy({ owner_id: userId });
+        res.status(200).json({ code: 200, data: products });
+      } catch (error) {
+        res.status(500).json({ code: 500, message: '获取用户产品时发生错误', error: error.message });
+      }
+    },
 
-  //     // Fetch all products using the extracted product_ids
-  //     const productsForSale = await Product.listByIdArray({ product_id: productIdsForSale });
+    // // 列出用户发布的待售products (List products for sale by a user)
+    // listUserSaleNFTs: async function (req, res, next) {
+    //   // 涉及到用户id的使用，路由中应该加入登录检查中间件
+    //   const userId = req.session.user_id;
+    //   try {
+    //     // 获取当前用户作为卖家的全部订单
+    //     const orders = await Order.listBy({ seller_id: userId });
 
-  //     res.status(200).json({ code: 200, data: productsForSale });
-  //   } catch (error) {
-  //     res.status(500).json({ code: 500, message: '获取用户待售产品时发生错误', error: error.message });
-  //   }
-  // },
+    //     // 将待售物品id提取出来放入一个数组中
+    //     const productIdsForSale = orders.map(o => o.product_id);
 
-  // // 查看NFT的交易历史 (View transaction history of an NFT)
-  // getNFTTransactionHistory: async function (req, res, next) {
-  //   const nftId = req.params.id;
-  //   try {
-  //       const transactionHistory = await Order.getNFTTransactionHistory(nftId);
-  //       res.json({ code: 200, data: transactionHistory });
-  //   } catch (error) {
-  //       // Handle error
-  //   }
-  // },
+    //     // Fetch all products using the extracted product_ids
+    //     const productsForSale = await Product.listByIdArray({ product_id: productIdsForSale });
 
-  // // 搜索NFT (Search for NFTs)
-  // searchNFTs: async function (req, res, next) {
-  //   const query = req.query.q;
-  //   try {
-  //       const searchResults = await Product.searchNFTs(query);
-  //       res.json({ code: 200, data: searchResults });
-  //   } catch (error) {
-  //       // Handle error
-  //   }
-  // },
-}
+    //     res.status(200).json({ code: 200, data: productsForSale });
+    //   } catch (error) {
+    //     res.status(500).json({ code: 500, message: '获取用户待售产品时发生错误', error: error.message });
+    //   }
+    // },
+
+    // // 查看NFT的交易历史 (View transaction history of an NFT)
+    // getNFTTransactionHistory: async function (req, res, next) {
+    //   const nftId = req.params.id;
+    //   try {
+    //       const transactionHistory = await Order.getNFTTransactionHistory(nftId);
+    //       res.json({ code: 200, data: transactionHistory });
+    //   } catch (error) {
+    //       // Handle error
+    //   }
+    // },
+
+    // // 搜索NFT (Search for NFTs)
+    // searchNFTs: async function (req, res, next) {
+    //   const query = req.query.q;
+    //   try {
+    //       const searchResults = await Product.searchNFTs(query);
+    //       res.json({ code: 200, data: searchResults });
+    //   } catch (error) {
+    //       // Handle error
+    //   }
+    // },
+  }
 
 module.exports = productController;
+
--- service/nftService.js
+++ service/nftService.js
@@ -2,16 +2,47 @@ const nft = require('../contract/nft');
 const rchainManager = require("../manager/rchainManager");
 
 async function mint(privateKey, nft_id, name, description, metadataUrl, coverImageUrl) {
-    const term = nft.mint_rho(nft_id, name, description, metadataUrl, coverImageUrl, "20231117");
-    var names = await rchainManager.deploy(`localhost`, privateKey, term, 1, 100000000);
-    var data = await rchainManager.getDataAtName(names[0]);
-    return new Promise((resolve) => {
+    const timestamp = Math.floor(Date.now() / 1000).toString();
+    const term = nft.mint_rho(nft_id, name, description, metadataUrl, coverImageUrl, timestamp);
+
+    try {
+        var names = await rchainManager.deploy(`localhost`, privateKey, term, 1, 100000000);
+        var data = await rchainManager.getDataAtName(names[0]);
         data = rchainManager.convertRChainData(data);
         console.log(data);
-        resolve(data);
-    })
+        return data; 
+    } catch (error) {
+        console.error("Error in minting NFT:", error);
+        throw new Error("Minting failed"); // 错误处理
+    }
 }
 
+async function listNftsForSale() {
+    const term = nft.getNftDataMap_rho();
+    try {
+        var names = await rchainManager.deploy(`localhost`, privateKey, term, 1, 100000000);
+        var data = await rchainManager.getDataAtName(names[0]);
+        data = convertRChainData(data);  // 使用提供的函数转换数据
+
+        // 筛选出所有待出售的NFT（即价格大于0）
+        const nftsForSale = Object.entries(data).reduce((acc, [id, nftDetails]) => {
+            // 使用转换后的数据格式
+            if (nftDetails.price > 0) {
+                acc[id] = nftDetails;
+            }
+            return acc;
+        }, {});
+
+        console.log(nftsForSale);
+        return nftsForSale; 
+    } catch (error) {
+        console.error("Error in listing NFTs for sale:", error);
+        throw new Error("Listing failed"); // 错误处理
+    }
+}
+
+
 module.exports = {
-    mint
+    mint,
+    listNftsForSale,
 };
