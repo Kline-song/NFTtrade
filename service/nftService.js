28c6d2bb24d8676800d2235065626f42b02e124e
Kline-song <527811545@qq.com>
2023-11-18 11:24:13 +0800

    main-goodupload

--- service/nftService.js
+++ service/nftService.js
@@ -2,12 +2,14 @@ const nft = require('../contract/nft');
 const rchainManager = require("../manager/rchainManager");
 
 async function mint(privateKey, nft_id, name, description, metadataUrl, coverImageUrl) {
-    const timestamp = Math.floor(Date.now() / 1000).toString();
+    const timestamp = new Date().valueOf();
+    console.log("timestamp=");
+    console.log(timestamp);
     const term = nft.mint_rho(nft_id, name, description, metadataUrl, coverImageUrl, timestamp);
 
     try {
         var names = await rchainManager.deploy(`localhost`, privateKey, term, 1, 100000000);
-        var data = await rchainManager.getDataAtName(names[0]);
+        var data = await rchainManager.getDataAtName('localhost',names[0]);
         data = rchainManager.convertRChainData(data);
         console.log(data);
         return data; 
